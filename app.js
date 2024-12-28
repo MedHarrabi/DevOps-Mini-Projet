d1304b1e885ee6880154f2c41d7655e0c574ee06
MedHarrabi <94125807+MedHarrabi@users.noreply.github.com>
2024-12-28 18:04:55 +0100

    .

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+app.use(cors());
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
